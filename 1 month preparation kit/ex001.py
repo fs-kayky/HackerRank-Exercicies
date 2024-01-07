5287b96b2260652a6da15a220c5a643d28124425
Kayky Rodrigues <109947756+fs-kayky@users.noreply.github.com>
2024-01-07 09:33:31 -0300

    first update exercicies

---
+++ 1 month preparation kit/ex001.py
@@ -0,0 +1,36 @@
+
+
+import math
+import os
+import random
+import re
+import sys
+
+def plusMinus(arr, n):
+
+    pos = []
+    neg = []
+    zero = []
+
+    for i in range(n):
+        if(arr[i] > 0):
+            pos.append(arr[i])
+        elif(arr[i] == 0):
+            zero.append(arr[i])
+        elif(arr[i]<0):
+            neg.append(arr[i])
+
+    print(f'{len(pos) / n:.6f}')
+    print(f'{len(neg) / n:.6f}')
+    print(f'{len(zero) / n:.6f}')
+            
+
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr, n)
